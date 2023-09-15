e344afc3179e4e7d8eb50d1ce840a85f1f6b4683
dunglv <dunglv202@gmail.com>
2023-09-15 07:18:04 +0700

    update data with websocket

--- src/components/DeviceControl/DeviceControl.js
+++ src/components/DeviceControl/DeviceControl.js
@@ -1,17 +1,31 @@
+import { Switch } from "@mui/material";
+import { useState } from "react";
 import Card from "../Ui/Card";
 import styles from "./DeviceControl.module.css";
-import { useState } from "react";
-import { Switch } from "@mui/material";
 
-function DeviceControl({ label, offIcon, onIcon }) {
+function DeviceControl({ deviceId, label, offIcon, onIcon }) {
   const [isOn, setIsOn] = useState(false);
+
+  const handleSwitch = async (e) => {
+    try {
+      let state = e.target.checked;
+      await fetch(`http://localhost:8080/api/devices/${deviceId}/state`, {
+        method: "POST",
+        body: state ? "on" : "off",
+      });
+      setIsOn(state);
+    } catch (e) {
+      alert("Could not change device state");
+      console.error(e);
+    }
+  };
 
   return (
     <Card className={styles["device-control-cont"]}>
       <div className={styles["device__icon"]}>{isOn ? onIcon : offIcon}</div>
       <h2 className={styles["device__name"]}>{label}</h2>
       <div className={styles["actions"]}>
-        <Switch onChange={(e) => setIsOn(e.target.checked)} />
+        <Switch checked={isOn} onChange={handleSwitch} />
       </div>
     </Card>
   );
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,29 +1,41 @@
-import { PiFanFill, PiFanLight, PiLightbulbFilamentFill, PiLightbulbFilamentLight } from "react-icons/pi";
-import { CiTempHigh } from "react-icons/ci";
+import { useState } from "react";
 import { BsDroplet, BsSun } from "react-icons/bs";
+import { CiTempHigh } from "react-icons/ci";
+import { PiFanFill, PiFanLight, PiLightbulbFilamentFill, PiLightbulbFilamentLight } from "react-icons/pi";
 import Chart from "../components/Chart/Chart";
 import DeviceControl from "../components/DeviceControl/DeviceControl";
 import Menu from "../components/Menu/Menu";
 import StatCard from "../components/StatCard/StatCard";
 import WelcomeCard from "../components/WelcomeCard/WelcomeCard";
+import useWebsocket from "../hooks/useWebsocket";
 import styles from "./Dashboard.module.css";
 
 function DashboardPage() {
+  const [sensorData, setSensorData] = useState({ temperature: "-", humidity: "-" });
+  const client = useWebsocket("ws://localhost:8080/websocket");
+  client.onConnect = () => {
+    client.subscribe("/topic/dht", (message) => {
+      setSensorData(JSON.parse(message.body));
+    });
+  };
+
   return (
     <div id={styles["content"]}>
       <Menu />
       <WelcomeCard />
-      <StatCard label="Temperature" icon={<CiTempHigh fontSize={46} />} value="22" unit="degree" />
-      <StatCard label="Humidity" icon={<BsDroplet fontSize={38} />} value="92" unit="percent" />
+      <StatCard label="Temperature" icon={<CiTempHigh fontSize={46} />} value={sensorData.temperature} unit="degree" />
+      <StatCard label="Humidity" icon={<BsDroplet fontSize={38} />} value={sensorData.humidity} unit="percent" />
       <StatCard label="Lighting" icon={<BsSun fontSize={38} />} value="50,000" unit="lux" />
       <Chart />
       <DeviceControl
         label="Led"
+        deviceId="led"
         offIcon={<PiLightbulbFilamentLight />}
         onIcon={<PiLightbulbFilamentFill className={styles["bulb-on"]} />}
       />
       <DeviceControl
         label="Fan"
+        deviceId="fan"
         offIcon={<PiFanLight className={styles["fan-off"]} />}
         onIcon={<PiFanFill className={styles["fan-on"]} />}
       />
