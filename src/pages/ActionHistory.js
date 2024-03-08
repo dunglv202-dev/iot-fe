a73857ecbe08a649740d92739b04060566df10ec
dunglv <dunglv202@gmail.com>
2024-03-08 21:27:27 +0700

    back to timelined history style

--- src/pages/ActionHistory.js
+++
@@ -1,5 +0,0 @@
-function ActionHistoryPage() {
-  return <h1>ActionHistoryPage</h1>;
-}
-
-export default ActionHistoryPage;
--- src/pages/ActionHistory/ActionHistory.js
+++ src/pages/ActionHistory/ActionHistory.js
@@ -1,68 +1,7 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { useEffect, useState } from "react";
-import { formatDate } from "../../utils/DateFormatter";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import DatePicker from "../../components/DatePicker/DatePicker";
-import Button from "../../components/Button/Button";
-import styles from "../../components/Table/Table.module.css";
-import pagination from "../../components/Pagination/Pagination.module.css";
-import { Pagination } from "@mui/material";
+import HistoryTimeline from "../../components/HistoryTimeline/HistoryTimeline";
 
 function ActionHistoryPage() {
-  const [sensorDataHistory, setSensorDataHistory] = useState([]);
-  const [page, setPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(1);
-
-  const handleChange = (_, value) => {
-    setPage(value);
-  };
-
-  useEffect(() => {
-    async function fetchHistory() {
-      let resp = await fetch(`http://localhost:8080/api/actions/history?page=${page - 1}`).then((res) => res.json());
-      setSensorDataHistory(resp.data);
-      setTotalPage(resp.totalPage);
-    }
-    fetchHistory();
-  }, [page]);
-
-  return (
-    <>
-      <div className={styles["header"]}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker label="From" />
-          <DatePicker label="To" />
-        </LocalizationProvider>
-        <Button>Filter</Button>
-      </div>
-      <Table>
-        <TableHead className={styles["table__head"]}>
-          <TableRow>
-            <TableCell>Device</TableCell>
-            <TableCell>Action</TableCell>
-            <TableCell>Timestamp</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody className={styles["table__body"]}>
-          {sensorDataHistory.map((history) => (
-            <TableRow key={history.timestamp}>
-              <TableCell>{history.deviceId}</TableCell>
-              <TableCell>{history.actionType}</TableCell>
-              <TableCell>{formatDate(new Date(history.timestamp))}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <div className={pagination["pagination"]}>
-        <Pagination count={totalPage} page={page} onChange={handleChange} />
-      </div>
-    </>
-  );
+  return <HistoryTimeline />;
 }
 
 export default ActionHistoryPage;
